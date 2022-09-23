c583c38129c62aec2f200da5c562047e22bc0cc2
Iftiker67 <65295289+Iftiker67@users.noreply.github.com>
2022-09-24 00:36:29 +0600

    Add files via upload

---
+++ Presentation_Layer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation_Layer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new UI.FormUI.POSTerminalForm());
+            //Application.Run(new UI.FormUI.MainForm());
+            Application.Run(new UI.FormUI.LoginForm());
+            //Application.Run(new UI.FormUI.RegistrationForm());
+            //Application.Run(new UI.PanelUI.PanelHome());
+        }
+    }
+}
